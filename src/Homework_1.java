31f40f627af508460f295cdbfa0c01106cd4b071
IliaSyrytskii <polonamocyi2525@gmail.com>
2022-11-23 21:57:17 +0300

    Commit 1

--- src/Homework_1.java
+++ src/Homework_1.java
@@ -4,6 +4,9 @@ public class Homework_1 {
     public static void master (String[] args){
         Scanner sc = new Scanner(System.in);
         String Name = sc.next();
-        System.out.println("Name " + Name);
+        for (int t = 0; t < 10; t++) {
+            System.out.println("Name " + Name);
+
+        }
     }
 }
