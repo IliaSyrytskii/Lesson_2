878cb1cdbd1fc292737984053db1973511b21c8d
IliaSyrytskii <polonamocyi2525@gmail.com>
2022-11-24 19:22:50 +0300

    Changed HW

--- src/Homework_1.java
+++ src/Homework_1.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
 
 public class Homework_1 {
-    public static void master (String[] args){
-        Scanner sc = new Scanner(System.in);
-        String Name = sc.next();
+    public static void main(String[] args) throws InterruptedException{
+            Scanner sc = new Scanner(System.in);
+            String Name = sc.next();
+
         for (int t = 0; t < 10; t++) {
+
+
+            Thread.sleep(2500);
             System.out.println("Name " + Name);
+
 
         }
     }
--- src/Homework_2.java
+++ src/Homework_2.java
@@ -4,14 +4,9 @@ public class Homework_2 {
     public static void main (String[] args) {
         System.out.println("Please, enter your name");
                 Scanner sc = new Scanner(System.in);
-                String name = sc.next();
         for (int n = 0; n < 10; n++) {
-            try {
-                Thread.sleep(2500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println("Name " + name);
+                String name = sc.next();
+                System.out.println("Name " + name);
         }
     }
 
